1d58da97a756e564b610d661d5772f323aa96a6e
Renan Caldas <renan@renancaldas.dev.br>
2024-02-04 21:30:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Usuários</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios
+      .get('${process.env.VUE_APP_BACKEND_URL}/api/users')
+      .then((response) => {
+        this.users = response.data
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar usuários:', error)
+      })
+  }
+}
+</script>
