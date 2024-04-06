06439042f95dc4386bf014362d15d5e8c0975048
Renan Caldas <renan@renancaldas.dev.br>
2024-04-06 20:05:31 -0300

    feat: init create dashboard view

---
+++ src/components/Navbar/ButtonsNavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ buttonText }}</p>
+    <a :href="buttonLink">
+      <i :class="buttonIcon"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true
+    },
+    buttonIcon: {
+      type: String,
+      required: true
+    },
+    buttonLink: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,4 +1,15 @@
 <template>
-  <h1>dashboard</h1>
-  <p>teste</p>
+  <aside class="bg-defaultBlack max-w-72 h-screen">
+    <div class="flex flex-col justify-center p-10">
+      <div>
+        <h3 class="text-2xl mb-10 font-bold text-white">
+          MyFinancy<span class="font-light">Easy</span>
+        </h3>
+      </div>
+      <div>
+        <p>Dashboard</p>
+        <p>Dashboard</p>
+      </div>
+    </div>
+  </aside>
 </template>
