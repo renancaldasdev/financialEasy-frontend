c89920d926355593209a876f465f21642aa67d48
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 16:08:05 -0300

    feat: create defaultLayout system

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen overflow-hidden">
+    <SideBarArea />
+
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <HeaderArea />
+      <main>
+        <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          <slot></slot>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SideBarArea from '@/components/Sidebar/SideBarArea.vue'
+import HeaderArea from '@/components/Header/HeaderArea.vue'
+</script>
