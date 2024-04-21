069de3461991498f50cf3012d8bd10609348dc8b
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 20:38:30 -0300

    feat: add icons systme

--- src/main.js
+++ src/main.js
@@ -8,11 +8,16 @@ import App from './App.vue'
 import router from './router'
 import '@/Assets/css/style.css'
 import '@/Assets/css/fonts.css'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import * as FaIcons from 'oh-vue-icons/icons/fa'
 
 const app = createApp(App)
+const Fa = Object.values({ ...FaIcons })
+addIcons(...Fa)
 
 app.use(createPinia())
 app.use(router)
 app.use(Toast, { position: POSITION.TOP_RIGHT })
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
