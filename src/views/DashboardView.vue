4f5526ad5eae3fc33f3e2c2153fa989cb2a71d27
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 16:09:12 -0300

    refactor: rfactor dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,7 @@
 <template>
-  <aside class="bg-defaultBlack max-w-72 h-screen">
-    <div class="flex flex-col justify-center p-10">
-      <div>
-        <h3 class="text-2xl mb-10 font-bold text-white">
-          MyFinancy<span class="font-light">Easy</span>
-        </h3>
-      </div>
-      <div>
-        <p>Dashboard</p>
-        <p>Dashboard</p>
-      </div>
-    </div>
-  </aside>
+  <DefaultLayout> </DefaultLayout>
 </template>
+
+<script setup>
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+</script>
