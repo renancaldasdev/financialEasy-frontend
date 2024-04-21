e1b0ebd9fb1de600efc284c8304a202c5bb8c496
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 20:37:17 -0300

    feat: input search

---
+++ src/components/Header/SearchFocus.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative">
+    <input
+      type="text"
+      placeholder="Pesquisar"
+      class="px-4 py-2 pr-10 rounded border border-gray-300 focus:outline-none focus:border-primaryColor font-regular text-gray02"
+    />
+    <div
+      class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 pointer-events-none"
+    >
+      <v-icon name="fa-search" />
+    </div>
+  </div>
+</template>
