0da0f25e13b887b5fe8d915f2cfac4db8408ac5f
Renan Caldas <renan@renancaldas.dev.br>
2024-04-21 16:10:01 -0300

    feat:create methods auths system

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,26 @@
+export const setCurrentUser = (user) => {
+  user = JSON.stringify(user)
+  localStorage.setItem('current_user', user)
+}
+
 export const getCurrentUser = () => {
   const token = localStorage.getItem('access_token')
   return token ? { Authorization: `Bearer ${token}` } : null
+}
+
+export const removeCurrentUser = () => {
+  localStorage.removeItem('current_user')
+}
+
+export const setToken = (token) => {
+  localStorage.setItem('access_token', token)
+}
+
+export const getToken = () => {
+  const token = localStorage.getItem('access_token')
+  return token
+}
+
+export const destroyToken = () => {
+  localStorage.removeItem('access_token')
 }
